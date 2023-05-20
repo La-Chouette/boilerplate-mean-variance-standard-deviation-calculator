a5f77a0ea6ecf1fa0c2d85752ad7e05b423d0aab
La-Chouette <65280624+La-Chouette@users.noreply.github.com>
2023-05-20 05:51:58 +0000

    mean calculation

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,8 +2,13 @@ import numpy as np
 
 def calculate(list):
   m = np.array(list)
-  print(m)
-
-
-
-    return calculations
+  print (m.reshape((3, 3)))
+  meanX = np.sum(m, axis = 0)/3
+  meanY = np.sum(m, axis = 1)/3
+  meanF =np.sum(m)/9
+  mean = [meanX,meanY,meanF]
+  values = [mean, 0]
+  parameters=['Mean','variance']
+  calculations = dict(zip(parameters,values))
+  
+  return calculations
